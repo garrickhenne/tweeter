3a081f9b2cfbae32872fa930e5521b818bcb8bd4
Garrick Henne <garrickhenne@outlook.com>
2023-10-10 16:33:53 -0700

    Refactor tweet rendering to renderTweets

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,6 +1,68 @@
+/* eslint-disable no-undef */
+// html string-literal highlighting
+const html = String.raw;
 /*
  * Client-side JS logic goes here
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
+const createTweetElement = (tweet) => {
+  const $tweet = $(html`
+    <article>
+      <header>
+        <div>
+          <img src="${tweet.user.avatars}" />
+          <p>${tweet.user.name}</p>
+        </div>
+        <p><strong>${tweet.user.handle}</strong></p>
+      </header>
 
+      <p>${tweet.content.text}</p>
+
+      <hr />
+      <footer>
+        <p>${tweet.created_at}</p>
+        <div>
+          <i class="fa-solid fa-flag"></i>
+          <i class="fa-sharp fa-solid fa-retweet"></i>
+          <i class="fa-solid fa-heart"></i>
+        </div>
+      </footer>
+    </article>`);
+
+  return $tweet;
+};
+
+const renderTweets = (tweets) => {
+  $('main').append(tweets.map(createTweetElement));
+};
+
+const data = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png"
+      ,
+      "handle": "@SirIsaac"
+    },
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1461116232227
+  },
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": "https://i.imgur.com/nlhLi3I.png",
+      "handle": "@rd"
+    },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  }
+];
+
+$(function() {
+  renderTweets(data);
+});
