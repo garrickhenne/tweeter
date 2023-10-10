00b1ad1f22c751f252da82e0cafd408a6638d88c
Garrick Henne <garrickhenne@outlook.com>
2023-10-09 23:18:10 -0700

    Implement character count update logic

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,18 @@
+$(document).ready(() => {
+  $('#tweet-text').on('input', function(e) {
+    const defaultColor = '#545149';
+    const exceededCharsColor = '#DD4C31';
+
+    let currLength = $(this).val().length;
+    let counterElement = $(this).parent().parent().children('div.button-text-layer').children('output.counter');
+    counterElement.val(140 - currLength);
+    if (counterElement.val() < 0) {
+      counterElement.css('color', exceededCharsColor);
+      return;
+    }
+
+    // From negative character count back to positive, we change back to default color.
+    if (counterElement.css('color') !== defaultColor)
+      counterElement.css('color', defaultColor);
+  });
+});
