15246a7c9b170c5eb5740255d0e49ea79a87c3fe
Garrick Henne <garrickhenne@outlook.com>
2023-10-10 17:11:03 -0700

    Add timeago library to format dates

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -21,7 +21,7 @@ const createTweetElement = (tweet) => {
 
       <hr />
       <footer>
-        <p>${tweet.created_at}</p>
+        <p>${timeago.format(tweet.created_at)}</p>
         <div>
           <i class="fa-solid fa-flag"></i>
           <i class="fa-sharp fa-solid fa-retweet"></i>
@@ -47,32 +47,12 @@ const renderTweets = (tweets) => {
   });
 };
 
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd"
-    },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-];
+const loadTweets = (callback) => {
+  return $.ajax('/tweets', { method: 'GET' })
+    .then(tweetsJSON => callback(tweetsJSON))
+    .catch(err => console.error('GET request to /tweets failed.', err));
+};
 
 $(function() {
-  renderTweets(data);
+  loadTweets(renderTweets);
 });
